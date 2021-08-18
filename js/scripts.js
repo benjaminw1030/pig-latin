f5dfb49d361f6fd8ba663cfdc660a2e17da4648a
Ben Wilson <benjaminw1030@gmail.com>
2021-08-18 13:42:19 -0700

    add functionality to move multiple consonants and add ay
    Co-authored-by: Paige Tiedeman <paigetiedeman@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,10 @@
 // output "way" at the end of all words beginning with a vowel (except y) //
 // not output "way" at the end of a word beginning with a consonant //
 // move the first consonant to the end of a word with only one consonant //
-// remove the first consonant from the beginning of a word with only one consonant
-// move the first consonants to the end of a word with multiple consonants
-// add ay after moving consonants
-// count y as a consonant, not a vowel
+// remove the first consonant from the beginning of a word with only one consonant //
+// move the first consonants to the end of a word with multiple consonants //
+// add ay after moving consonants //
+// count y as a consonant, not a vowel //
 // move q and u together as "qu" and add way
 // do this regardless of capitalization
 // stretch goal: ignore punctuation
@@ -16,19 +16,31 @@
 // Business Logic
 
 
-function addWay (word) {
-  const array = word.split("");
-  const vowelArray = ["a", "i", "o", "u", "e"];
-  for (let i = 0; i < vowelArray.length; i += 1) {
-    if (vowelArray[i] === array[0]) {
-      word += ("way");
-      return word
-      break;
+function addWay(word) {
+  let wordArray = word.split("");
+  const vowels = ["a", "i", "o", "u", "e"];
+  for (let i = 0; i < vowels.length; i += 1) {
+    if (vowels[i] === wordArray[0]) {
+      return word + "way";
     } else {
-      array.push(array[0]);
-      newWord = array.join('').substr(1);
+      let consonants = []
+      for (let i = 0; i < wordArray.length; i += 1) {
+        if (!vowels.includes(wordArray[i])) {
+          consonants.push(wordArray[i]);
+        } else {
+          break;
+        }
+      }
+      sliceWord = wordArray.slice(consonants.length).concat(consonants);
+      const newWord = sliceWord.join('');
       return newWord + "ay";
     }
-  } 
+  }
 }
-addWay("hello")
+addWay("threat");
+
+// wordArray.forEach(function (element) {
+//   if (!uniqueArray.includes(element)) {
+//     uniqueArray.push(element);
+//   }
+// });
