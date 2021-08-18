40fefbab21e0f260b3cba06c9ffe709559604d22
Ben Wilson <benjaminw1030@gmail.com>
2021-08-18 16:43:44 -0700

    clean up logic, remove unneccessary break

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,3 @@
-// function should:
-// output "way" at the end of a word //
-// output "way" at the end of a word once //
-// output "way" at the end of a word beginning with a vowel //
-// output "way" at the end of all words beginning with a vowel (except y) //
-// not output "way" at the end of a word beginning with a consonant //
-// move the first consonant to the end of a word with only one consonant //
-// remove the first consonant from the beginning of a word with only one consonant //
-// move the first consonants to the end of a word with multiple consonants //
-// add ay after moving consonants //
-// count y as a consonant, not a vowel //
-// move q and u together as "qu" and add ay //
-// do this regardless of capitalization //
-
 // Business Logic
 
 function pigLatinWord(word) {
@@ -33,8 +19,6 @@ function pigLatinWord(word) {
           }
           else if (!vowels.includes(wordArray[i])) {
             consonants.push(wordArray[i]);
-          } else { 
-            break;
           }
         }
         sliceWord = wordArray.slice(consonants.length).concat(consonants);
